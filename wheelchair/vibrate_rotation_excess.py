4177da3f44b777969e4608a76280da46e8f1f687
Samira <spnz.dario@gmail.com>
2019-03-11 16:40:09 +0100

    d

--- wheelchair/vibrate_rotation_excess.py
+++ wheelchair/vibrate_rotation_excess.py
@@ -58,14 +58,23 @@ def handle_rotation_data(handle, value_bytes):
     find_or_create("Left Wheel Rotation",
                    PropertyType.TWO_DIMENSIONS).update_values(rotation_values)
 
-    if ((rotation_values[0] or rotation_values[1]) % ROTVAL == 0) and not vib:
+    if ((rotation_values[0] % ROTVAL == 0) and not vib:
         ser.write('1'.encode())
         time.sleep(2)
         ser.write('0'.encode())
         global vib
-        print("not vib yet %s" % str(vib))
+        #print("not vib yet %s" % str(vib))
         #vib = True
-        print("after nudge %s" % str(vib))
+        #print("after nudge %s" % str(vib))
+
+    if ((rotation_values[1] % ROTVAL == 0) and not vib:
+        ser.write('1'.encode())
+        time.sleep(2)
+        ser.write('0'.encode())
+        global vib
+        #print("not vib yet %s" % str(vib))
+        #vib = True
+        #print("after nudge %s" % str(vib))
 
 def keyboard_interrupt_handler(signal_num):
     """Make sure we close our program properly"""

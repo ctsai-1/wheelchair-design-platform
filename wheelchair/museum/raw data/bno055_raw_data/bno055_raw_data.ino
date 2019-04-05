be4b631bc97427b4f7351944968c9a4280f4b75d
Dariosapienza <spnz.dario@gmail.com>
2019-04-05 17:39:56 +0200

    k

--- wheelchair/museum/raw data/bno055_raw_data/bno055_raw_data.ino
+++ wheelchair/museum/raw data/bno055_raw_data/bno055_raw_data.ino
@@ -19,7 +19,7 @@
 
 
 // Here, we set the delay, in ms,  between fresh samples (the sample rate)
-#define DELAY_MS 1000
+#define DELAY_MS 20
 
 // Creating our sensor object to handle the sensor, with initialization 12345
 Adafruit_BNO055 bno = Adafruit_BNO055(12345);
@@ -27,38 +27,38 @@ Adafruit_BNO055 bno = Adafruit_BNO055(12345);
 // Here we create our raw data variables
 imu::Vector<3> euler_vector;            // for reading euler vector values
                                         // (values in Euler angles or 'degrees', from 0..359)
-imu::Vector<3> magnetometer_vector;     // for reading magnetometer vector values
-                                        // (values in uT, micro Tesla)
-imu::Vector<3> gyroscope_vector;        // for reading gyroscope vector values
+//imu::Vector<3> magnetometer_vector;     // for reading magnetometer vector values
+                                        // (values in uT, micro Tesla)*/
+//imu::Vector<3> gyroscope_vector;        // for reading gyroscope vector values
                                         // (values in rps, radians per second)
-imu::Vector<3> accelerometer_vector;    // for reading accelerometer vector values
+/*imu::Vector<3> accelerometer_vector;    // for reading accelerometer vector values
                                         // (values in m/s^2)
 imu::Vector<3> linear_accel_vector;     // for reading linear acceleration vector values
                                         // (values in m/s^2)
 imu::Vector<3> gravity_vector;          // for reading gravity vector values
-                                        // (values in m/s^2)
+                                        // (values in m/s^2)*/
 
 
-imu::Quaternion quaternion;             // for reading quaternion values
-int8_t temperature;                     // for reading temperature values (8 bytes)
+//imu::Quaternion quaternion;             // for reading quaternion values
+//int8_t temperature;                     // for reading temperature values (8 bytes)
 
 
-
+char orientation= "frame_orientation";
 // Arduino setup function (automatically called at startup)
 void setup(void)
 {
   Serial.begin(9600);
-  Serial.println("Lets begin our Orientation Sensor Test"); Serial.println("");
+  //Serial.println("Lets begin our Orientation Sensor Test"); Serial.println("");
 
   /* Initialise the sensor */
   if(!bno.begin())
   {
     /* Problem detecting the BNO055 ... wiring error */
-    Serial.print("No BNO055 is detected ... Check your wiring or I2C ADDR!");
+   // Serial.print("No BNO055 is detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
 
-  delay(1000);
+  //delay(1000);
 
   /* Display some basic information on this sensor */
   displaySensorDetails();
@@ -85,31 +85,31 @@ void loop(void)
   
   // Extracting raw data vectors 
   euler_vector= bno.getVector(Adafruit_BNO055::VECTOR_EULER);            
-  magnetometer_vector = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);     
-  gyroscope_vector = bno.getVector(Adafruit_BNO055:: VECTOR_GYROSCOPE);;        
-  accelerometer_vector = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);   
+  //magnetometer_vector = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER); */    
+  //gyroscope_vector = bno.getVector(Adafruit_BNO055:: VECTOR_GYROSCOPE);;        
+  /*accelerometer_vector = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);   
   linear_accel_vector = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
-  gravity_vector = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
+  gravity_vector = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);*/
 
   // Extracting other raw data
-  quaternion = bno.getQuat();    // get quaternion helper function         
-  temperature = bno.getTemp();   // get temperature helper function
+  //quaternion = bno.getQuat();    // get quaternion helper function         
+  //temperature = bno.getTemp();   // get temperature helper function
 
 
   /* Display the floating point data */
   
-  Serial.println("euler:");
-  Serial.println("");
-  Serial.print("X: ");
+  
+  Serial.print("frame_orientation");
+  Serial.print(",");
   Serial.print(euler_vector.x());
-  Serial.print(" Y: ");
+  Serial.print(",");
   Serial.print(euler_vector.y());
-  Serial.print(" Z: ");
+  Serial.print(",");
   Serial.print(euler_vector.z());
-  Serial.println("");
+  Serial.print(",");
   Serial.println("");
 
-  delay(50);
+  /*delay(50);
 
   Serial.println("magnetometer:");
   Serial.println("");
@@ -122,21 +122,25 @@ void loop(void)
   Serial.println("");
   Serial.println("");
 
-  delay(50);
+  delay(50);*/
 
-  Serial.println("gyroscope:");
+  //GIROSCOPIO
+
+  /*Serial.println("gyroscope:");
   Serial.println("");
   Serial.print("X: ");
-  Serial.print(gyroscope_vector.x());
-  Serial.print(" Y: ");
-  Serial.print(gyroscope_vector.y());
-  Serial.print(" Z: ");
-  Serial.print(gyroscope_vector.z());
-  Serial.println("");
-  Serial.println("");
-  delay(50);
   
-  Serial.println("accelerometer:");
+  Serial.print(gyroscope);
+  Serial.print(",");
+  Serial.println(gyroscope_vector.x());
+  Serial.print(",");
+  Serial.println(gyroscope_vector.y());
+  Serial.print(",");
+  Serial.println(gyroscope_vector.z());
+  Serial.print(",");
+    //delay(50);
+  
+  /*Serial.println("accelerometer:");
   Serial.println("");
   Serial.print("X: ");
   Serial.print(accelerometer_vector.x());
@@ -170,10 +174,10 @@ void loop(void)
   Serial.print(gravity_vector.z());
   Serial.println("");
   Serial.println("");
-  delay(50);
+  delay(50);*/
 
   /* Display the quaternion data, 4 decimal places */
-  Serial.println("Quaternion:");
+  /*Serial.println("Quaternion:");
   Serial.print("qW: ");
   Serial.print(quaternion.w(), 4);
   Serial.print(" qX: ");
@@ -191,16 +195,16 @@ void loop(void)
   Serial.print(temperature);
   Serial.println(" C");
   Serial.println("");
-  Serial.println("");
+  Serial.println("");*/
   
   /* Optional: Display calibration status */
-  // displayCalStatus();
+  displayCalStatus();
 
   /* Optional: Display sensor status (for debug purposes only) */
-  //displaySensorStatus();
+  displaySensorStatus();
 
   /* New line for the next sample */
-  Serial.println("");
+  //Serial.println("");
 
   /* Wait the specified delay before requesting nex data */
   delay(DELAY_MS) ;
@@ -212,7 +216,7 @@ void displaySensorDetails(void)
 {
   sensor_t sensor;
   bno.getSensor(&sensor);
-  Serial.println("------------------------------------");
+  /*Serial.println("------------------------------------");
   Serial.print  ("Sensor:       "); Serial.println(sensor.name);
   Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
   Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
@@ -221,7 +225,7 @@ void displaySensorDetails(void)
   Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
   Serial.println("------------------------------------");
   Serial.println("");
-  delay(500);
+  delay(500);*/
 }
 
 
@@ -234,7 +238,7 @@ void displaySensorStatus(void)
   bno.getSystemStatus(&system_status, &self_test_results, &system_error);
 
   /* Display the results in the Serial Monitor */
-  Serial.println("");
+  /*Serial.println("");
   Serial.print("System Status: 0x");
   Serial.println(system_status, HEX);
   Serial.print("Self Test:     0x");
@@ -242,7 +246,7 @@ void displaySensorStatus(void)
   Serial.print("System Error:  0x");
   Serial.println(system_error, HEX);
   Serial.println("");
-  delay(500);
+  delay(500);*/
 }
 
 
@@ -258,19 +262,19 @@ void displayCalStatus(void)
   bno.getCalibration(&system, &gyro, &accel, &mag);
 
   /* The data should be ignored until the system calibration is > 0 */
-  Serial.print("\t");
+ /* Serial.print("\t");
   if (!system)
   {
     Serial.print("! ");
-  }
+  }*/
 
   /* Display the individual values */
-  Serial.print("Sys:");
+  /*Serial.print("Sys:");
   Serial.print(system, DEC);        // Printing decimal values  
   Serial.print(" G:");
   Serial.print(gyro, DEC);
   Serial.print(" A:");
   Serial.print(accel, DEC);
   Serial.print(" M:");
-  Serial.print(mag, DEC);
+  Serial.print(mag, DEC);*/
 }

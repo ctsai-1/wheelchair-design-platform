e2de08182c40b5256f7cfc3c870d0c0da5033b46
SAMIRA MICCOLIS <samiramiccolis@gmail.com>
2019-04-05 18:16:32 +0200

    TRY AND EXCPET

--- wheelchair/python/serial_to_dcdhub.py
+++ wheelchair/python/serial_to_dcdhub.py
@@ -18,6 +18,8 @@ load_dotenv()
 THING_ID = os.environ['THING_ID']
 THING_TOKEN = os.environ['THING_TOKEN']
 
+DATA_PROP_NAME = "frame_orientation-ab9e"
+
 # Instantiate a thing with its credential
 my_thing = Thing(thing_id=THING_ID, token=THING_TOKEN)
 
@@ -25,7 +27,9 @@ my_thing = Thing(thing_id=THING_ID, token=THING_TOKEN)
 # i.e. retrieving its information from the hub
 my_thing.read()
 
-# Start reading the serial port
+prop_data = my_thing.find_or_create_property(DATA_PROP_NAME, PropertyType.THREE_DIMENSIONS)
+
+
 ser = serial.Serial(
     port = os.environ['SERIAL'],
     baudrate = 9600,
@@ -39,19 +43,23 @@ def serial_to_property_values():
     # If the line is not empty
     if len(line_bytes) > 0:
         # Convert the bytes into string
-        line = line_bytes.decode('utf-8')
+        try:
+            line = line_bytes.decode('utf-8')
+        except UnicodeDecodeError:
+            line = "0,0,0,0,0,0,0,0,0,0,0,0"
         # Split the string using commas as separator, we get a list of strings
-        values = line.split(',')
-        # Use the first element of the list as property id
-        property_id = values.pop(0)
-        # Get the property from the thing
-        prop = my_thing.properties[property_id]
-        # If we find the property, we update the values (rest of the list)
-        if prop is not None:
-            prop.update_values([float(x) for x in values])
-        # Otherwise, we show a warning
-        else:
-            print('Warning: unknown property ' + property_id)
+        str_values = line.split(',')
+        # Remove the first id
+        str_values.pop(0)
+        # Transform the array of string values into float values (numbers)
+        values = [float(x) for x in str_values]
+
+        # Update values of data and label properties (send them to the DCD Hub)
+        # With the same timestamp, so we can easily connect label and raw data later
+
+        prop_data.update_values(values)
+
+
     # Finally, we call this method again
     serial_to_property_values()
 

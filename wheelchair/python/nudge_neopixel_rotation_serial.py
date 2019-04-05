8e0abafe315844ba79c83b48ecd2a1c70cdc27b8
SAMIRA MICCOLIS <samiramiccolis@gmail.com>
2019-04-05 15:55:36 +0200

    trial serial

--- wheelchair/python/nudge_neopixel_rotation_serial.py
+++ wheelchair/python/nudge_neopixel_rotation_serial.py
@@ -107,3 +107,31 @@ left_wheel.subscribe(GATT_CHARACTERISTIC_ROTATION, callback=handle_rotation_data
 
 # Register our Keyboard handler to exit
 signal.signal(signal.SIGINT, keyboard_interrupt_handler)
+
+
+# Read the next line from the serial port
+# and update the property values
+def serial_to_property_values():
+    # Read one line
+    line_bytes = ser.readline()
+    # If the line is not empty
+    if len(line_bytes) > 0:
+        # Convert the bytes into string
+        line = line_bytes.decode('utf-8')
+        # Split the string using commas as separator, we get a list of strings
+        values = line.split(',')
+        # Use the first element of the list as property id
+        property_id = values.pop(0)
+        # Get the property from the thing
+        prop = my_thing.properties[property_id]
+        # If we find the property, we update the values (rest of the list)
+        if prop is not None:
+            prop.update_values([float(x) for x in values])
+        # Otherwise, we show a warning
+        else:
+            print('Warning: unknown property ' + property_id)
+    # Finally, we call this method again
+    serial_to_property_values()
+
+
+serial_to_property_values()
